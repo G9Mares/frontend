a528cd906679892d44adaf90c67903a447b3b98f
G9Mares <gustavo99.mares@gmail.com>
2026-07-26 19:54:30 -0600

    feat: :white_check_mark: Creacion de entornos de desarrollo y produccion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { API_BASE_URL } from './core/utils/api-base-url.token';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    { provide: API_BASE_URL, useValue: environment.apiBaseUrl },
+  ],
 };
